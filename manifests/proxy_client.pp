5579053bc1e1e35c1068c1a8f95625408673ca67
Yoann Laissus <yoann.laissus@gmail.com>
2013-05-25 23:04:51 +0200

    ensure support in proxy_client

--- manifests/proxy_client.pp
+++ manifests/proxy_client.pp
@@ -1,9 +1,11 @@
 class apt::proxy_client(
   $proxy = 'http://localhost',
   $port = '3142',
+  $ensure = present
 ){
 
   apt_conf { '20proxy':
     content => template('apt/20proxy.erb'),
+    ensure => $ensure
   }
 }
